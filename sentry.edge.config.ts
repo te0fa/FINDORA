c590da43f52388f21671af2fe152c5b8cdcc7cdb
Developer <developer@example.com>
2026-08-19 12:01:11 +0300

    fix(sentry): lower sampling rates and disable client debug

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,7 +5,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console when Sentry is initialized.
   debug: false,
